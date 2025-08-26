258bcd148fba3b98b2733cb3d908b95f25394669
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-26 16:24:55 +0200

    refactor(game logic): refractored game logic.
    See title.

--- game.js
+++ game.js
@@ -13,26 +13,33 @@ const winConditions = {
   "scissors": "paper",
 };
 
+const reset = () => {
+  result.forEach((res) => {
+    res.classList.add("hidden");
+  });
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     players.forEach((player) => {
       player.classList.add("shake");
-      player.addEventListener("animationend", () => {
-        player.classList.remove("shake");
-      });
     });
-    result.forEach((res) => {
-      res.classList.add("hidden");
-    });
+    reset();
     let playerChoice = button.classList[0];
     let chooser = Math.floor(Math.random() * 3);
     let computerChoice = choices[chooser];
     let matchup = [playerChoice, computerChoice];
     console.log(matchup);
-    if (playerChoice === computerChoice) {
-      document.getElementById("draw").classList.remove("hidden");
-    } else {
-      return winConditions[playerChoice] === computerChoice ? win.classList.remove("hidden") : loose.classList.remove("hidden");
-    }
+    players.forEach((player) => { 
+      player.addEventListener("animationend", () => {
+        player.classList.remove("shake");
+        reset();
+        if (playerChoice === computerChoice) {
+          return document.getElementById("draw").classList.remove("hidden");
+        } else {
+          return winConditions[playerChoice] === computerChoice ? win.classList.remove("hidden") : loose.classList.remove("hidden");
+        }
+      })
+    });
   });
 });
